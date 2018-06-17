06959d7a5a80ffdd7827171cc684fa2007fdc81e
Fredy Rivera <fredyjesus2013@gmail.com>
2018-06-17 00:57:22 -0500

    Base de Datos remota

--- app/src/main/java/com/example/fredy/inventoryapp/LoginRequest.java
+++ app/src/main/java/com/example/fredy/inventoryapp/LoginRequest.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LoginRequest extends StringRequest{
-    private static final String LOGIN_REQUEST_URL = "http://192.168.1.3/Login.php";
+    private static final String LOGIN_REQUEST_URL = "https://inventoryapphola.000webhostapp.com/Login.php";
     private Map<String,String> params;
     public LoginRequest(String username,String password,Response.Listener<String> listener){
         super(Method.POST,LOGIN_REQUEST_URL,listener,null);
--- app/src/main/java/com/example/fredy/inventoryapp/RegisterRequest.java
+++ app/src/main/java/com/example/fredy/inventoryapp/RegisterRequest.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterRequest extends StringRequest {
-    private static final String REGISTER_REQUEST_URL = "http://192.168.1.3/Register.php";
+    private static final String REGISTER_REQUEST_URL = "https://inventoryapphola.000webhostapp.com/Register.php";
     private Map<String,String> params;
     public RegisterRequest(String name, String username, int age, String password, String email, Response.Listener<String> listener){
         super(Method.POST,REGISTER_REQUEST_URL,listener,null);
